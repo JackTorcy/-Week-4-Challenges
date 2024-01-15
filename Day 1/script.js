bb185c295c61d60ebc8c3f2f9db10cb428d48ebe
JackTorcy <Jack@torcy.com>
2024-01-15 10:59:27 +0000

    Day 1
    Day 1 upload

---
+++ Day 1/script.js
@@ -0,0 +1,9 @@
+// Display to console
+console.log('Welcome to JavaScript');
+
+// Display as error and warning
+console.error('Welcome to JavaScript');
+console.warn('Welcome to JavaScript');
+
+// Clear console
+console.clear();
