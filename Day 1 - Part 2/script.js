4ece4af12d20100a3d34b0eff139dccd120bea57
JackTorcy <Jack@torcy.com>
2024-01-15 11:31:07 +0000

    Part 2 upload
    Changed name of Part 1 and added Part 2

---
+++ Day 1 - Part 2/script.js
@@ -0,0 +1,27 @@
+//! ***  TASK ***
+ 
+ /*
+ 1. I would like you to Create a variable called fastFood and assign the value Pizza,
+    another variable called favColor with the value Red and a third
+    variable called favDrink with the value lemonade.
+ 
+ 2. Console log the favDrink variable to show it's value.
+ 
+ 3. Change the value of the favFood variable to "Pasta", console log it's value.
+ 
+ 4. Create a variable, call it whatever you want and assign the value of your brithday.
+ */
+//1 - create Variables
+let fastFood='Pizza';
+let favColor='Red';
+let favDrink='Lemonade';
+
+//2 - Log favDrink
+console.log(favDrink);
+
+//3 - Change value and log
+favFood='Pasta'
+console.log(fastFood)
+
+//4 - Create birthday and assign variable
+const birthday='5th June 2000'
